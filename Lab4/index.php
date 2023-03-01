3267d4d11a1f09931348c412045b8973f32042aa
Artem Khametov <khametovartem@gmail.com>
2023-03-01 19:06:44 +0300

    lab4

--- Lab4/index.php
+++ Lab4/index.php
@@ -21,64 +21,81 @@
         </div>
     </header>
     <main class="function__4">
-        <form action="index.php" method="post">
-            <input class="function__input" type="text" name="equation" value="">
-            <button class="form__button" type="submit">
-                Subscribe
-            </button>
-        </form>
+        <div class="function_4__wrapper">
+            <form action="index.php" method="post">
+                <fieldset class="forma__name">
+                    <legend class="name__text">
+                        Ваше имя <span class="zvez">*</span>
+                    </legend>
+                    <label class="name__label">
+                        <span class="visually-hidden">
+                        Type Your Name
+                        </span>
+                        <input class="function__input" placeholder="X/8=6" type="" name="equation" value="">
+                    </label>
+                </fieldset>
+                <button class="form__button" type="submit">
+                    Subscribe
+                </button>
+            </form>
+            <div class="function_4__php">
+                <?php  
+                    if (isset($_POST['equation'])) {
+                        $str = $_POST['equation'];
+                        $str = preg_replace("/\s+/", "", $str);
+                        if (strlen($str)>5) {
+                            echo 'Введите однозначные числа';
+                        }elseif ($str[2]==0){
+                            echo 'Error';
+                        }elseif (strlen($str)>0) {
+                            for( $i = 0 ; $i < strlen($str); $i=$i+1){
+                                if ($str[$i]=='-'){
+                                    $index = $i;
+                                    $a='-';
+                                }
+                                if ($str[$i]=='*'){
+                                    $index = $i;
+                                    $a='*';
+                                }
+                                if ($str[$i]=='/'){
+                                    $index = $i;
+                                    $a='/';
+                                }
+                                if ($str[$i]=='+'){
+                                    $index = $i;
+                                    $a='+';
+                                }
+                                if ($str[$i]=='='){
+                                    $index_equally = $i;
+                                }
+                            }
+                            $difference=$index_equally - $index-1;
+                            $number = (strlen($str)) - $index_equally;
+                            if ($difference==1){
+                                $b=$str[$number];
+                            }
+                            $c=$str[$index_equally+1];
+                            $b=(int)$b;
+                            $c=(int)$c;
+                            if ($a=='*'){
+                                echo $c / $b;
+                            }
+                            if ($a=='+'){
+                                echo $c - $b;
+                            }
+                            if ($a=='/'){
+                                echo $c * $b;
+                            }
+                            if ($a=='-'){
+                                echo $c + $b;
+                            }
+                        };
+                    };
+                ?>
+            </div>
+            
+        </div>
         
-        <?php  
-            if (isset($_POST['equation'])) {
-                $str = $_POST['equation'];
-                $str = preg_replace("/\s+/", "", $str);
-                if (strlen($str)>5) {
-                    echo 'Введите однозначное число';
-                }elseif (strlen($str)>0) {
-                    for( $i = 0 ; $i < strlen($str); $i=$i+1){
-                        if ($str[$i]=='-'){
-                            $index = $i;
-                            $a='-';
-                        }
-                        if ($str[$i]=='*'){
-                            $index = $i;
-                            $a='*';
-                        }
-                        if ($str[$i]=='/'){
-                            $index = $i;
-                            $a='/';
-                        }
-                        if ($str[$i]=='+'){
-                            $index = $i;
-                            $a='+';
-                        }
-                        if ($str[$i]=='='){
-                            $index_equally = $i;
-                        }
-                    }
-                    $difference=$index_equally - $index-1;
-                    $number = (strlen($str)) - $index_equally;
-                    if ($difference==1){
-                        $b=$str[$number];
-                    }
-                    $c=$str[$index_equally+1];
-                    $b=(int)$b;
-                    $c=(int)$c;
-                    if ($a=='*'){
-                        echo $c / $b;
-                    }
-                    if ($a=='+'){
-                        echo $c - $b;
-                    }
-                    if ($a=='/'){
-                        echo $c * $b;
-                    }
-                    if ($a=='-'){
-                        echo $c + $b;
-                    }
-                };
-            };
-        ?>
     </main>
     <footer class="footer">
         <div class="footer__wrapper">
